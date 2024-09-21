36a716eb4859921919c3fd9c8fa92150a488554f
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2024-09-21 23:23:00 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// var container = document.querySelector(".container");
+// console.log(container.children); // ====> HTMLCollection [p,p,p] ===> tags only
+// console.log(container.childNodes); // =====> NodeList [text,p,text,p,text,p,text] ===> tags, text, spaces, anyshit
+// console.log(container.children[1].children[0]);
+// console.log(container.nextElementSibling);
+// console.log(container.nextSibling);
+// console.log(container.parentElement);
+// console.log(container.parentNode);
+// console.log(container.previousElementSibling);
+
+
+// !              HTMLCollection                                NodeList  
+// ?            getElementByTagName()                        querySelectorAll()       
+// ?        changes the elements "live"                doesnt change the element "not live"
+
+
+
+
+//ANCHOR - 
